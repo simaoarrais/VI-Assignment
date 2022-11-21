f100777073747e8f62a65b693a5d51ffb3621fbd
Mariana Rosa <marianarosa@ua.pt>
2022-11-21 21:25:40 +0000

    Categories added

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,9 +5,11 @@ var videos_title = new Array();
 var videos_id = new Array();
 var videos_published = new Array();
 var videos_keyword = new Array();
+var videos_keyword_unique = [...new Set(videos_keyword)];
 var videos_likes = new Array();
 var videos_comments = new Array();
 var videos_views = new Array();
+
 
 // Iterate through the CSV file and save data
 async function parseData(file) {
@@ -20,12 +22,14 @@ async function parseData(file) {
     videos_likes.push(data["Likes"]);
     videos_comments.push(data["Comments"]);
     videos_views.push(data["Views"]);
+    videos_keyword_unique = [...new Set(videos_keyword)];
+
   });
 }
 
 // Build Top Videos Table
 async function createTops() {
-
+  console.log("Creating Top Videos Table");
   // Clone the video views array and sort it
   var sorted_views = videos_views.map(function(e){return e;});
   sorted_views.sort(function(a, b){return a - b});
@@ -55,6 +59,7 @@ async function createTops() {
 }
 
 function addCellToTable (table_row, info) {
+
   var table_cell = document.createElement('td');
   table_cell.innerText = info;
   table_row.appendChild(table_cell);
@@ -64,6 +69,67 @@ function capitalizeFirstLetter (string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function barPlotCategories() {
+
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#barPlotCategories")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+
+  // sort data
+  data.sort(function(b, a) {
+    return a.Value - b.Value;
+  });
+
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(function(d) { return d.Country; }))
+    .padding(0.2);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 13000])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Bars
+  svg.selectAll("mybar")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.Country); })
+      .attr("y", function(d) { return y(d.Value); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.Value); })
+      .attr("fill", "#69b3a2")
+
+})
+
+
+
+
+
+}
 function drawPieChart() {
 
   // Create the data table.
@@ -95,12 +161,39 @@ function drawPieChart() {
 	chart.draw(data, options);
 }
 
+function addOptionsDropdown (options) {
+  console.log("INIT")
+  var myDiv = document.getElementById("divCategories");
+
+  //Create array of options to be added
+  //Create and append select list
+  var selectList = document.createElement("select");
+  selectList.setAttribute("id", "mySelect");
+  myDiv.appendChild(selectList);
+
+
+  console.log("Adding Options to Dropdown");
+  console.log("Options", options);
+  for (var i = 0; i < options.length; i++) {
+    var option = document.createElement("option");
+    option.setAttribute("value", options[i]);
+    option.text = options[i];
+    selectList.appendChild(option);
+  }
+}
+
 // Init
 async function init() {
   await parseData(file_path);
+  console.log("Im here")
   createTops();
   google.charts.load('current', {'packages':['corechart']});
   google.charts.setOnLoadCallback( drawPieChart );
+  addOptionsDropdown(videos_keyword_unique);
+  barPlotCategories();
+  addCellToTable() ; //problemas com esta funcao
+
+
 }
 
 init();
